c7b2b31ec1a52eb9f1edded4fb21cb573a8c72a6
hlli8 <lg95312lg@outlook.com>
2020-01-16 21:19:49 +0800

    利用Caffe做人脸检测
    Face detection with OpenCV and deep learning
    为在windows下运行，将需要参数入参的地方修改为False，直接利用绝对路径指定参数
    效果很好

--- Classifying_Images_Using_DeepLearning&OpenCV/detect_faces.py
+++ Classifying_Images_Using_DeepLearning&OpenCV/detect_faces.py
@@ -5,7 +5,7 @@ using OpenCV and GoogleLeNet(pre-trained on ImageNet) using the Caffe framework.
 @Author: HLLI8
 @Date: 2020-01-13 21:41:01
 @LastEditors  : HLLI8
-@LastEditTime : 2020-01-16 20:40:23
+@LastEditTime : 2020-01-16 21:12:08
 '''
 import sys
 sys.path.append ("D:/ProgramFile/Anaconda/Lib/site-packages") 
@@ -19,9 +19,9 @@ import cv2
 
 #construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True, help="path to input image")
-ap.add_argument("-p", "--prototxt", required=True, help="path to Caffe 'deploy' prototxt file")
-ap.add_argument("-m", "--model", required=True, help="path to Caffe pre-trained model")
+ap.add_argument("-i", "--image", required=False, help="path to input image")
+ap.add_argument("-p", "--prototxt", required=False, help="path to Caffe 'deploy' prototxt file")
+ap.add_argument("-m", "--model", required=False, help="path to Caffe pre-trained model")
 ap.add_argument("-c", "--confidence", type=float, default=0.5, help="minimum probability to filter weak detections")
 args = vars(ap.parse_args())
 
@@ -29,7 +29,7 @@ args = vars(ap.parse_args())
 print("[INFO] loading model...")
 prototxt = "E:/PythonWorkSpace/DeepLearningWithOpenCV/Classifying_Images_Using_DeepLearning&OpenCV/model/deploy.prototxt.txt"
 model = "E:/PythonWorkSpace/DeepLearningWithOpenCV/Classifying_Images_Using_DeepLearning&OpenCV/model/res10_300x300_ssd_iter_140000.caffemodel"
-image = "E:/PythonWorkSpace/DeepLearningWithOpenCV/Classifying_Images_Using_DeepLearning&OpenCV/image/iron_chic.jpg"
+image = "E:/PythonWorkSpace/DeepLearningWithOpenCV/Classifying_Images_Using_DeepLearning&OpenCV/image/p_01.jpg"
 confidence_threshold = 0.5
 #net = cv2.dnn.readNetFromCaffe(args["prototxt"], arg["model"])
 net = cv2.dnn.readNetFromCaffe(prototxt, model)
