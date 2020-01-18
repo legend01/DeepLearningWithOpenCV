70878542175108b7c3e23e77c34f502a9f44cb40
hlli8 <lg95312lg@outlook.com>
2020-01-18 21:36:01 +0800

    适应在Windows下运行此代码
    将参数修改，修改为必要的参数为False，加载协议和模型

--- Classifying_Images_Using_DeepLearning&OpenCV/detect_faces_video.py
+++ Classifying_Images_Using_DeepLearning&OpenCV/detect_faces_video.py
@@ -4,8 +4,13 @@
 @Author: HLLI8
 @Date: 2020-01-16 21:30:51
 @LastEditors  : HLLI8
-@LastEditTime : 2020-01-18 20:30:07
+@LastEditTime : 2020-01-18 21:12:06
 '''
+import sys
+sys.path.append ("D:/ProgramFile/Anaconda/Lib/site-packages") 
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' #只显示warning和Error
+
 #import the necessary packages
 from imutils.video import VideoStream
 import numpy as np
@@ -16,14 +21,18 @@ import cv2
 
 #construct the arguments parse and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-p", "--prototxt", required=True, help="path to Caffe 'deploy' prototxt file")
-ap.add_argument("-m", "--model", required=True, help="path to Caffe pre-trained model")
+ap.add_argument("-p", "--prototxt", required=False, help="path to Caffe 'deploy' prototxt file")
+ap.add_argument("-m", "--model", required=False, help="path to Caffe pre-trained model")
 ap.add_argument("-c", "--confidence", type=float, default=0.5, help="minimum probability to filter weak detections")
 args = vars(ap.parse_args())
 
 #load our serialized model from disk
 print("[INFO] loading model...")
-net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
+prototxt = "E:/PythonWorkSpace/DeepLearningWithOpenCV/Classifying_Images_Using_DeepLearning&OpenCV/model/deploy.prototxt.txt"
+model = "E:/PythonWorkSpace/DeepLearningWithOpenCV/Classifying_Images_Using_DeepLearning&OpenCV/model/res10_300x300_ssd_iter_140000.caffemodel"
+confidence_threshold = 0.5
+#net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
+net = cv2.dnn.readNetFromCaffe(prototxt, model)
 
 #initialize the video stream and allow the camera sensor to warm up
 print("[INFO] starting video stream....")
@@ -35,16 +44,16 @@ while True:
     #grab the frame from the threaded video stream and resize it
     #to have a maximum width of 400 pixels
     frame = vs.read()
-    frame = imutils.resize(frame, width=400)
+    frame = imutils.resize(frame, width=800) #FIXME:width=400使得图框太小
 
-    #grap the frame dimensions and convert it to a blob
+    #grab the frame dimensions and convert it to a blob
     (h, w) = frame.shape[:2]
     blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 1.0, (300, 300), (104.0, 177.0, 123.0))
 
     #pass the blob through the network and obtain the detection and predictions
     net.setInput(blob)
     detections = net.forward()
-    
+
     #loop over the detections
     for i in range(0, detections.shape[2]):
         #extract the confidence associated with the prediction
@@ -52,10 +61,11 @@ while True:
 
         #filter out weak detections by ensuring the "confidence" is greater than the
         #minimum confidence
-        if confidence < args["confidence"]:
+        #if confidence < args["confidence"]:
+        if confidence < confidence_threshold:
             continue
 
-        #conpute the (x, y)-coordinates of the bounding box for the
+        #compute the (x, y)-coordinates of the bounding box for the
         #object
         box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
         (startX, startY, endX, endY) = box.astype("int")
@@ -64,18 +74,16 @@ while True:
         #probability
         text = "{:.2f}%".format(confidence * 100)
         y = startY - 10 if startY - 10 > 10 else startY + 10
-        cv2.rectangle(frame, (startX, startY), (endX, endY),
-            (0, 0, 255), 2)
-        cv2.putText(frame, text, (startX, y), cv2.FONT_HERSHEY_SIMPLEX,
-            0.45, (0, 0, 255), 2)
-        
+        cv2.rectangle(frame, (startX, startY), (endX, endY), (0, 0, 255), 2)
+        cv2.putText(frame, text, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+
         #show the output frame
         cv2.imshow("Frame", frame)
         key = cv2.waitKey(1) & 0xFF
 
         #if the "q" key was pressed, break from the loop
-        if key == ord("q"):
-			break
+        if (key == ord("q")):
+            break
 #do a bit of cleanup
 cv2.destroyAllWindows()
 vs.stop()
