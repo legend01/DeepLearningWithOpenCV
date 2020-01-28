00f38ce211d386e2b877b5e80a67475df9a21251
hlli8 <lg95312lg@outlook.com>
2020-01-28 13:57:27 +0800

    导入imutils和CV2,加载图片，读取图片尺寸，导入第三方库读取路径

--- OpenCV_Tutotial/opencv_guide_01.py
+++ OpenCV_Tutotial/opencv_guide_01.py
@@ -4,15 +4,27 @@
 @Author: HLLI8
 @Date: 2020-01-28 13:26:22
 @LastEditors  : HLLI8
-@LastEditTime : 2020-01-28 13:29:57
+@LastEditTime : 2020-01-28 13:57:02
 '''
+import sys
+sys.path.append ("D:/ProgramFile/Anaconda/Lib/site-packages") 
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' #只显示warning和Error
+
 #import the necessary packages
 import imutils
 import cv2
 
 #load the input image and show its dimensions, keeping in mind that images are represented as a multi-dimensions NumPy array
 #with shape no. rows(height) x no. colums (width) x no. channels (depth)
-image = cv2.imread()
+image = cv2.imread("jp.png")
+(h, w, d) = image.shape
+print("width={}, height={}, depth={}".format(w, h, d))
+
+#display the image to our screen --we will need to click the window open by OpenCV and press a key on our keyboard to continue execution
+cv2.imshow("Image", image)
+cv2.waitKey(0)
+
 
 
 
