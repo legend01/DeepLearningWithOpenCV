674a737406b3114efe08c5a0eb7bebb57bfe10cd
hlli8 <lg95312lg@outlook.com>
2020-01-28 13:31:58 +0800

    学习OpenCV
    导入OpenCV库函数，读取照片，读取照片格式

---
+++ OpenCV_Tutotial/opencv_guide_01.py
@@ -0,0 +1,41 @@
+'''
+@Description: 学习OpenCV
+@version: 
+@Author: HLLI8
+@Date: 2020-01-28 13:26:22
+@LastEditors  : HLLI8
+@LastEditTime : 2020-01-28 13:29:57
+'''
+#import the necessary packages
+import imutils
+import cv2
+
+#load the input image and show its dimensions, keeping in mind that images are represented as a multi-dimensions NumPy array
+#with shape no. rows(height) x no. colums (width) x no. channels (depth)
+image = cv2.imread()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
