d29882b1854ae1a1365afeeaead21093fa9948bc
hlli8 <lg95312lg@outlook.com>
2020-03-14 12:06:42 +0800

    计算俄罗斯方块 图像转换灰度值 边缘检测
    1.利用OpenCV将图像转换为灰度
           2.执行边缘检测
           3.灰度图像阈值
           4.查找、计数和绘制等高线
           5.腐蚀和膨胀
           6.掩盖图像

---
+++ OpenCV_Tutotial/opencv_count numberOfBlocks.py
@@ -0,0 +1,51 @@
+'''
+@Description: OpenCV计算俄罗斯方块
+@version: 
+@Author: HLLI8
+@Date: 2020-03-14 11:59:52
+@LastEditors: HLLI8
+@LastEditTime: 2020-03-14 12:05:43
+'''
+""" 
+@TODO: 1.利用OpenCV将图像转换为灰度
+       2.执行边缘检测
+       3.灰度图像阈值
+       4.查找、计数和绘制等高线
+       5.腐蚀和膨胀
+       6.掩盖图像 
+"""
+import sys
+sys.path.append ("D:/ProgramFile/Anaconda/Lib/site-packages") 
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' #只显示warning和Error
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
