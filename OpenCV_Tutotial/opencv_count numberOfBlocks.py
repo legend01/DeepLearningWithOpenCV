b482bb6d087efc353e14a2fb16cb276be3888254
hlli8 <lg95312lg@outlook.com>
2020-03-14 15:41:06 +0800

    检测并绘画轮廓

--- OpenCV_Tutotial/opencv_count numberOfBlocks.py
+++ OpenCV_Tutotial/opencv_count numberOfBlocks.py
@@ -4,7 +4,7 @@
 @Author: HLLI8
 @Date: 2020-03-14 11:59:52
 @LastEditors: HLLI8
-@LastEditTime: 2020-03-14 14:11:41
+@LastEditTime: 2020-03-14 15:25:36
 '''
 """ 
 @TODO: 1.利用OpenCV将图像转换为灰度
@@ -61,8 +61,17 @@ thresh = cv2.threshold(gray, 225, 255, cv2.THRESH_BINARY_INV)[1]
 cv2.imshow("Thresh", thresh)
 cv2.waitKey(0)
 
+#find contours of the foreground objects in the threshold image
+cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+cnts = imutils.grab_contours(cnts)
+output = image.copy()
 
-
+#loop over the contours
+for c in cnts:
+    #draw each contour on the output image with a 3px thick purple outline, then display the output contours one at a time
+    cv2.drawContours(output, [c], -1, (240, 0, 159), 3)
+    cv2.imshow("Conrours", output)
+    cv2.waitKey(0)
 
 
 
