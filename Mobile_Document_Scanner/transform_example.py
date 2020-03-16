a048245b73463087a02aca8a9c625a040ec3c1a4
hlli8 <lg95312lg@outlook.com>
2020-03-16 15:09:58 +0800

    测试transform.py文件算法
    运行参数补充

--- Mobile_Document_Scanner/transform_example.py
+++ Mobile_Document_Scanner/transform_example.py
@@ -4,12 +4,21 @@
 @Author: HLLI8
 @Date: 2020-03-16 14:58:08
 @LastEditors: HLLI8
-@LastEditTime: 2020-03-16 14:58:51
+@LastEditTime: 2020-03-16 15:08:31
 '''
 import sys
 sys.path.append ("D:/ProgramFile/Anaconda/Lib/site-packages") 
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' #只显示warning和Error
  
+from outsideLibrary.transform import four_point_transform
+import numpy as np
+import argparse
+import cv2
 
- 
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", help="path to the image file")
+ap.add_argument("-c", "--coords", help="comma seperated list of source points")
+args = vars(ap.parse_args())
+
+
