c66ef021d868c0ea18689b228f7318a282486bea
hlli8 <lg95312lg@outlook.com>
2020-03-16 16:48:56 +0800

    检测最大的边缘

--- Mobile_Document_Scanner/scan.py
+++ Mobile_Document_Scanner/scan.py
@@ -4,7 +4,7 @@
 @Author: HLLI8
 @Date: 2020-03-14 17:27:41
 @LastEditors: HLLI8
-@LastEditTime: 2020-03-16 16:04:24
+@LastEditTime: 2020-03-16 16:44:07
 '''
 import sys
 sys.path.append ("D:/ProgramFile/Anaconda/Lib/site-packages") 
@@ -38,5 +38,28 @@ edged = cv2.Canny(gray, 75, 200)
 print("STEP 1: Edge Detection")
 cv2.imshow("Image", image)
 cv2.imshow("Edged", edged)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+#find the contours in the edged image, keeping only the largest ones, and initialize the screen contour
+cnts = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+cnts = imutils.grab_contours(cnts)
+cnts = sorted(cnts, key=cv2.contourArea, reverse=True)[:5]
+
+#loop over the contours
+for c in cnts:
+    #approximate the contour
+    peri = cv2.arcLength(c, True)
+    approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+
+    #如果大概的边缘有四个点，就能假设已经发现图像范围
+    if len(approx) == 4:
+        screenCnt = approx
+        break
+
+#显示文本的边缘线
+print("STEP 2: Find contours of paper")
+cv2.drawContours(image, [screenCnt], -1, (0, 255, 0), 2)
+cv2.imshow("Outline", image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
