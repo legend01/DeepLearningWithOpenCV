931bec603708b689553cec6382639473d480a9c5
hlli8 <lg95312lg@outlook.com>
2020-03-16 16:10:22 +0800

    导入测试图片
    代码中修复缺陷 代码中获取图片地址

--- Mobile_Document_Scanner/scan.py
+++ Mobile_Document_Scanner/scan.py
@@ -4,7 +4,7 @@
 @Author: HLLI8
 @Date: 2020-03-14 17:27:41
 @LastEditors: HLLI8
-@LastEditTime: 2020-03-16 15:48:26
+@LastEditTime: 2020-03-16 16:04:24
 '''
 import sys
 sys.path.append ("D:/ProgramFile/Anaconda/Lib/site-packages") 
@@ -19,17 +19,18 @@ import cv2
 import imutils
 
 ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True, help="Path to the image to be scanned")
+ap.add_argument("-i", "--image", required=False, help="Path to the image to be scanned")
 args = vars(ap.parse_args())
-
+image_path = "E:/PythonWorkSpace/DeepLearningWithOpenCV/Mobile_Document_Scanner/image/receipt.jpg"
 #load the image and compute the ratio of the old height to the new height, clone it, and resize it
-image = cv2.imread(args["image"])
+#image = cv2.imread(args["image"])
+image = cv2.imread(image_path)
 ratio = image.shape[0] / 500
 orig = image.copy()
 image = imutils.resize(image, height=500)
 
 #convert the image to grayscale, blur it, and find edges in the image
-gray = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 gray = cv2.GaussianBlur(gray, (5, 5), 0)
 edged = cv2.Canny(gray, 75, 200)
 
@@ -37,5 +38,5 @@ edged = cv2.Canny(gray, 75, 200)
 print("STEP 1: Edge Detection")
 cv2.imshow("Image", image)
 cv2.imshow("Edged", edged)
-cv2.waitkey(0)
+cv2.waitKey(0)
 cv2.destroyAllWindows()
