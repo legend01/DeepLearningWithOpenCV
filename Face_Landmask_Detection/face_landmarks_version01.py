1209cca94b5c802bbdd88c0e3e90dd1e4e88989e
hlli8 <lg95312lg@outlook.com>
2020-03-19 21:08:26 +0800

    面部检测 面部标记

--- Face_Landmask_Detection/face_landmarks_version01.py
+++ Face_Landmask_Detection/face_landmarks_version01.py
@@ -4,7 +4,7 @@
 @Author: HLLI8
 @Date: 2020-03-19 19:08:59
 @LastEditors: HLLI8
-@LastEditTime: 2020-03-19 19:57:30
+@LastEditTime: 2020-03-19 21:07:50
 '''
 import sys
 sys.path.append ("D:/ProgramFile/Anaconda/Lib/site-packages") 
@@ -33,4 +33,30 @@ image = imutils.resize(image, width=500)
 gray = cv2.cvtColor(image, cv2.Color_BGR2GRAY)
 
 #detector faces in the grayscale image
+''' 
+@note: 在图片中处理面部边界
+@Author: HLLI8
+@Date: 2020-03-19 20:48:14
+'''
 rects = detector(gray, 1)
+
+for (i, rect) in enumerate(rects):
+    #面部区域检测面部标记，转换面部标记(x, y)坐标为numpy列表
+    shape = predictor(gray, rect) 
+    shape = face_utils.shape_to_np(shape)
+    
+    #转换dlib`s 矩形为openCV形式边界框[(x, y, w, h)]然后画出面部边界框
+    (x, y, w, h) = face_utils.rect_to_bb(rect)
+    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    #show the face number
+    cv2.putText(image, "Face #{}".format(i +  1), (x - 10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+    #循环(x, y)坐标为面部区域做标记
+    for(x, y) in shape:
+        cv2.cicle(image, (x, y), 1, (0, 0, 255), -1)
+    
+#展示面部检测 + 面部区域标记图片输出
+cv2.imshow("Output", image)
+cv2.waitKey(0)
+
