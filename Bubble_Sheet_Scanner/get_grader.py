6188b35a910f1eb41b6a6a286176a1cd770b63a7
hlli8 <lg95312lg@outlook.com>
2020-03-23 17:37:55 +0800

    找边缘排序 定四个点

--- Bubble_Sheet_Scanner/get_grader.py
+++ Bubble_Sheet_Scanner/get_grader.py
@@ -4,7 +4,7 @@
 @Author: HLLI8
 @Date: 2020-03-23 16:15:45
 @LastEditors: HLLI8
-@LastEditTime: 2020-03-23 16:44:20
+@LastEditTime: 2020-03-23 17:10:02
 '''
 import sys
 sys.path.append ("D:/ProgramFile/Anaconda/Lib/site-packages") 
@@ -30,3 +30,24 @@ image = cv2.imread(args["image"])
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 blurred = cv2.GaussianBlur(gray, (5,5), 0)
 edged = cv2.Canny(blurred, 75, 200)
+
+#找到边缘轮廓，初始化文档的边缘
+cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_AIMPLE)
+cnts = imutils.grab_contours(cnts)
+docCnt = None
+
+#确保至少一个边缘被找到
+if len(cnts) > 0:
+    #根据其大小用递减的方式排序
+    cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
+
+    #loop over the sorted Contours
+    for c in cnts:
+        #估计边缘
+        preri = cv2.arcLength(c, True)
+        approx = cv2.approxPolyDP(c, 0.02*peri, True)
+
+        #假设有四个点，就认为发现了paper
+        if len(approx) == 4:
+            docCnt = approx
+            break
