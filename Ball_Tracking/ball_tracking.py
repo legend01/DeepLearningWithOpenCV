acd5308c80942ab4e3b84f01bfbc81e37a6909c9
hlli8 <lg95312lg@outlook.com>
2020-03-28 16:37:21 +0800

    图像追踪可运行

--- Ball_Tracking/ball_tracking.py
+++ Ball_Tracking/ball_tracking.py
@@ -4,7 +4,7 @@
 @Author: HLLI8
 @Date: 2020-03-25 18:07:00
 @LastEditors: HLLI8
-@LastEditTime: 2020-03-26 13:39:18
+@LastEditTime: 2020-03-28 16:36:20
 '''
 import sys
 sys.path.append ("D:/ProgramFile/Anaconda/Lib/site-packages") 
@@ -56,6 +56,56 @@ while True:
 
     #construct a mask for the color "green", then perform a series of dilations and erosions to remove any small
     #blobs left in the mask
-    mask = cv2.inRange(hav, greenLower, greenUpper)
+    mask = cv2.inRange(hsv, greenLower, greenUpper)
     mask = cv2.erode(mask, None, iterations=2)
     mask = cv2.dilate(mask, None, iterations=2)
+    
+    #find contours in the mask and initialize the current (x, y) center of the ball
+    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    center = None
+
+    #only proceed if at least one contour was found
+    if len(cnts) > 0:
+        #find the largest contour int the mask, then use it to compute the minimum enclosing circle and centroid
+        c = max(cnts, key=cv2.contourArea)
+        ((x, y), radius) = cv2.minEnclosingCircle(c)
+        M = cv2.moments(c)
+        center = (int(M["m10"]/M["m00"]), int(M["m01"]/M["m00"])) #计算图像的质心
+
+        #only proceed if the radius meets a minimum size
+        if radius > 10:
+            #draw the cicle and centroid on the frame, then update the list of tracked points
+            cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
+            cv2.circle(frame, center, 5, (0, 0, 255), -1)
+        
+    #update the points queue
+    pts.appendleft(center)
+    
+    #loop over the set of tracked points
+    for i in range(1, len(pts)):
+        #if either of the tracked pints are None, ignore them
+        if pts[i - 1] is None or pts[i] is None:
+            continue
+        
+        #otherwise, compute the thickness of the line and draw the connecting lines
+        thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
+        cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
+
+    #show the frame to our screen
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
+    
+    #if the "q" key is pressed, stop the loop
+    if key == ord("q"):
+        break
+
+#if we are not using a video file, stop the camera video streams
+if not args.get("video", False):
+    vs.stop()
+#otherwise, release the camera
+else:
+    vs.release()
+
+#close all windows
+cv2.destroyAllWindows()
