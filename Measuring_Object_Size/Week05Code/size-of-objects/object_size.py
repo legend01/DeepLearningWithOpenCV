75d0a31659782b67f324db631feaa3724431849a
hlli8 <lg95312lg@outlook.com>
2020-04-01 15:13:15 +0800

    输入参数 定义一个获取中点的函数

--- Measuring_Object_Size/Week05Code/size-of-objects/object_size.py
+++ Measuring_Object_Size/Week05Code/size-of-objects/object_size.py
@@ -1,8 +1,3 @@
-# USAGE
-# python object_size.py --image images/example_01.png --width 0.955
-# python object_size.py --image images/example_02.png --width 0.955
-# python object_size.py --image images/example_03.png --width 3.5
-
 # import the necessary packages
 from scipy.spatial import distance as dist
 from imutils import perspective
--- Measuring_Object_Size/measuring_object_size.py
+++ Measuring_Object_Size/measuring_object_size.py
@@ -4,7 +4,7 @@
 @Author: HLLI8
 @Date: 2020-04-01 14:55:24
 @LastEditors: HLLI8
-@LastEditTime: 2020-04-01 14:58:04
+@LastEditTime: 2020-04-01 15:05:36
 '''
 import sys
 sys.path.append ("D:/ProgramFile/Anaconda/Lib/site-packages") 
@@ -18,4 +18,12 @@ import numpy as np
 import argparse
 import imutils
 import cv2
+
+def midpoint(ptA, ptB):
+    return ((ptA[0] + ptB[0]) * 0.5, (ptA[1] + ptB[1]) * 0.5)
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True, help="path to the input image")
+ap.add_argument("-w", "--width", type=float, required=True, help="width of the left-most object in the image(in inches)")
+args = vars(ap.parse_args())
 
