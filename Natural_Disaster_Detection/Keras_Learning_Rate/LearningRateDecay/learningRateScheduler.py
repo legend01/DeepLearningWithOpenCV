69b16e17afcd9224938146edb29bbdabddd27442
hlli8 <lg95312lg@outlook.com>
2020-04-15 15:33:15 +0800

    基于步骤的学习率表

--- Natural_Disaster_Detection/Keras_Learning_Rate/LearningRateDecay/learningRateScheduler.py
+++ Natural_Disaster_Detection/Keras_Learning_Rate/LearningRateDecay/learningRateScheduler.py
@@ -4,7 +4,7 @@
 @Author: HLLI8
 @Date: 2020-04-15 10:36:18
 @LastEditors: HLLI8
-@LastEditTime: 2020-04-15 11:03:56
+@LastEditTime: 2020-04-15 15:18:09
 '''
 import matplotlib.pyplot as plt
 import numpy as np
@@ -19,3 +19,16 @@ class LearningRateDecay:
         plt.title(title)
         plt.xlabel("Epoch #")
         plt.ylabel("Learning Rate")
+
+class StepDecay(LearningRateDecay):
+    def __init__(self, initAlpha=0.01, factor=0.25, dropEvery=10):
+        self.initAlpha = initAlpha
+        self.factor = factor 
+        self.dropEvery = dropEvery
+
+    def __call__(self, epoch):
+        #计算当前迭代的学习率
+        exp np.floor((1+epoch)/self.dropEvery)
+        alpha = self.initAlpha * (self.factor**exp)
+        #返回学习率
+        return float(alpha)
