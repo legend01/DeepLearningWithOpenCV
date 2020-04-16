d4558adbf5dd8e55a441c339fd05477f399b4d15
hlli8 <lg95312lg@outlook.com>
2020-04-16 17:04:49 +0800

    导入库和定义变量

--- Natural_Disaster_Detection/Keras_CyclicalLearningRate/keras-cyclical-learning-rates/MyCode/train_FIFAR10.py
+++ Natural_Disaster_Detection/Keras_CyclicalLearningRate/keras-cyclical-learning-rates/MyCode/train_FIFAR10.py
@@ -1,4 +1,17 @@
+import os
 
+#初始化标签
+CLASSES=["airplane", "automobile", "bird", "cat", "deer", "dog", "frog", "horse", "ship", "truck"]
+
+MIN_LR = 1e-7
+MAX_LR = 1e-2
+BATCH_SIZE = 64
+STEP_SIZE = 8
+CLR_METHOD = "triangular"
+NUM_EPOCHS = 96
+
+TRAINING_PLOT_PATH = os.path.sep.join(["E:/PythonWorkSpace/DeepLearningWithOpenCV/Natural_Disaster_Detection/Keras_CyclicalLearningRate/keras-cyclical-learning-rates/MyCode/Output", "training_plot.png"])
+CLR_PLOT_PATH = os.path.join(["E:/PythonWorkSpace/DeepLearningWithOpenCV/Natural_Disaster_Detection/Keras_CyclicalLearningRate/keras-cyclical-learning-rates/MyCode/Output"], "clr_plot.png")
 
 
 
