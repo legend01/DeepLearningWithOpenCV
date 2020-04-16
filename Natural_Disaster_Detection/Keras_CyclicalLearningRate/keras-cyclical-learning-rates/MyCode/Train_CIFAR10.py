2d05b5bd465e7ff2130e85e675fe7fe16b1a4e97
hlli8 <lg95312lg@outlook.com>
2020-04-16 17:42:25 +0800

    导入数据集CIFAR10

--- Natural_Disaster_Detection/Keras_CyclicalLearningRate/keras-cyclical-learning-rates/MyCode/Train_CIFAR10.py
+++ Natural_Disaster_Detection/Keras_CyclicalLearningRate/keras-cyclical-learning-rates/MyCode/Train_CIFAR10.py
@@ -12,7 +12,22 @@ from keras.datasets import cifar10
 import matplotlib.pyplot as plt
 import numpy as np
 
+print("[INFO] loading CIFAR-10 data...")
+((trainX, trainY), (testX, testY)) = cifar10.load_data()
+trainX = trainX.astype("float")
+testY = testX.astype("float")
 
+mean = np.mean(trainX, axis=0)
+trainX -= mean
+testX -= mean
+
+lb = LabelBinarizer()
+trainY = lb.fit_transform(trainY)
+testY = lb.transform(testY)
+
+aug = ImageDataGenerator(width_shift_range=0.1, height_shift_range=0.1, horizontal_flip=True, fill_mode="nearest")
+
+print
 
 
 
