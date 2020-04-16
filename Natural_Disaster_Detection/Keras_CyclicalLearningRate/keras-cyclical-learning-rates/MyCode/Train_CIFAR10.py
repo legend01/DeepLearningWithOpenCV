a9d8e2e4d15d9a0eb059c7b576c8b36419f4945f
hlli8 <lg95312lg@outlook.com>
2020-04-16 17:21:56 +0800

    导入必要的库

--- Natural_Disaster_Detection/Keras_CyclicalLearningRate/keras-cyclical-learning-rates/MyCode/Train_CIFAR10.py
+++ Natural_Disaster_Detection/Keras_CyclicalLearningRate/keras-cyclical-learning-rates/MyCode/Train_CIFAR10.py
@@ -1,8 +1,25 @@
-'''
-@Description: 
-@version: 
-@Author: HLLI8
-@Date: 2020-04-16 17:08:40
-@LastEditors: HLLI8
-@LastEditTime: 2020-04-16 17:08:41
-'''
+import matplotlib
+matplotlib.use("Agg")
+
+from LibraryFile.minigooglenet import MiniGoogLeNet
+from LibraryFile.clr_callback import CyclicLR
+from LibraryFile import config
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.metrics import classification_report
+from keras.preprocessing.image import ImageDataGenerator
+from keras.optimizers import SGD
+from keras.datasets import cifar10
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+
+
+
+
+
+
+
+
+
+
