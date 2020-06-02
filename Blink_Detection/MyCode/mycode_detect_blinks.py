2e5523936103ffa8df7f76f67be7a9f46bfa8a69
hlli8 <lg95312lg@outlook.com>
2020-06-02 09:20:51 +0800

    导入库文件

--- Blink_Detection/MyCode/mycode_detect_blinks.py
+++ Blink_Detection/MyCode/mycode_detect_blinks.py
@@ -4,5 +4,21 @@
 @Author: HLLI8
 @Date: 2020-06-02 09:14:01
 @LastEditors: HLLI8
-@LastEditTime: 2020-06-02 09:14:34
+@LastEditTime: 2020-06-02 09:18:13
 '''
+import sys
+sys.path.append ("D:/ProgramFile/Anaconda/Lib/site-packages") 
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' #只显示warning和Error
+
+from scipy.spatial import distance as dist
+from imutils.video import FileVideoStream
+from imutils.video import VideoStream
+from imutils import face_utils
+import numpy as np
+import argparse
+import imutils
+import time
+import dlib
+import cv2
+
