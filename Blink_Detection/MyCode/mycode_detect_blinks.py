2c0c0dae766d6839d505f8e1f43c4601922e8c0e
hlli8 <lg95312lg@outlook.com>
2020-06-02 09:15:18 +0800

    眨眼识别

---
+++ Blink_Detection/MyCode/mycode_detect_blinks.py
@@ -0,0 +1,8 @@
+'''
+@Description: Detecting blinks with facial landmarks and opencv
+@version: 
+@Author: HLLI8
+@Date: 2020-06-02 09:14:01
+@LastEditors: HLLI8
+@LastEditTime: 2020-06-02 09:14:34
+'''
---
+++ TinyYOLO/Week07code/tiny-yolo-ncs/detect_objectByTinyYOLO.py
@@ -0,0 +1,14 @@
+'''
+@Description: 使用TinyYOLO算法
+@version: 
+@Author: HLLI8
+@Date: 2020-05-29 16:09:37
+@LastEditors: HLLI8
+@LastEditTime: 2020-05-29 16:10:31
+'''
+import sys
+sys.path.append ("D:/ProgramFile/Anaconda/Lib/site-packages") 
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' #只显示warning和Error
+
+from 
