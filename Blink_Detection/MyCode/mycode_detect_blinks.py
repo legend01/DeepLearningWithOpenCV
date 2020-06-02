4dde0b1af52debf5ece2cd8c996f5e7983ac0a84
hlli8 <lg95312lg@outlook.com>
2020-06-02 09:27:11 +0800

    眼睛纵横比计算

--- Blink_Detection/MyCode/mycode_detect_blinks.py
+++ Blink_Detection/MyCode/mycode_detect_blinks.py
@@ -4,7 +4,7 @@
 @Author: HLLI8
 @Date: 2020-06-02 09:14:01
 @LastEditors: HLLI8
-@LastEditTime: 2020-06-02 09:18:13
+@LastEditTime: 2020-06-02 09:26:43
 '''
 import sys
 sys.path.append ("D:/ProgramFile/Anaconda/Lib/site-packages") 
@@ -21,4 +21,18 @@ import imutils
 import time
 import dlib
 import cv2
+
+def eye_aspect_ratio(eye):
+    #计算垂直坐标之间的欧几里得距离
+    A = dist.euclidean(eye[1], eye[5])
+    B = dist.euclidean(eye[2], eye[4])
+
+    #计算水平眼镜区域之间的欧几里得距离
+    C = dist.euclidean(eye[0], eye[3])
+    
+    #计算眼睛的纵横比
+    ear = (A + B)/(2.0 * C)
+
+    #返回眼睛的纵横比
+    return ear
 
