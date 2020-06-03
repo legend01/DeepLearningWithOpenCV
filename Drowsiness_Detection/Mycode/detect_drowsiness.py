b956f8e98c98e778c1d955d17ff9ce9aba75a9c9
hlli8 <lg95312lg@outlook.com>
2020-06-03 14:42:02 +0800

    导入必要的库文件

--- Drowsiness_Detection/Mycode/detect_drowsiness.py
+++ Drowsiness_Detection/Mycode/detect_drowsiness.py
@@ -4,10 +4,22 @@
 @Author: HLLI8
 @Date: 2020-06-03 14:37:19
 @LastEditors: HLLI8
-@LastEditTime: 2020-06-03 14:38:17
+@LastEditTime: 2020-06-03 14:41:36
 '''
 import sys
 sys.path.append ("D:/ProgramFile/Anaconda/Lib/site-packages") 
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' #只显示warning和Error
+
+from scipy.spatial import distance as dist
+from imutils.video import VideoStream
+from imutils import face_utils
+from threading import Thread
+import numpy as np
+import playsound
+import argparse
+import imutils
+import time
+import dlib
+import cv2
 
