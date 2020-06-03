7c5a857daa389bfdf501dea8217e16c9f32e2bf6
hlli8 <lg95312lg@outlook.com>
2020-06-03 14:39:44 +0800

    通过openCV睡意检测

---
+++ Drowsiness_Detection/Mycode/detect_drowsiness.py
@@ -0,0 +1,13 @@
+'''
+@Description: Building the drowsiness detector with openCV
+@version: 
+@Author: HLLI8
+@Date: 2020-06-03 14:37:19
+@LastEditors: HLLI8
+@LastEditTime: 2020-06-03 14:38:17
+'''
+import sys
+sys.path.append ("D:/ProgramFile/Anaconda/Lib/site-packages") 
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' #只显示warning和Error
+
