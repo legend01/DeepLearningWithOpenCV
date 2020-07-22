7ac197ada83f66412dd8a1e58389a07b32a015d8
hlli8 <lg95312lg@outlook.com>
2020-07-22 10:01:32 +0800

    物体跟踪 第一个版本

---
+++ Face_Landmask_Detection/Object_Tracking/Tracking_01.py
@@ -0,0 +1,13 @@
+'''
+@Description: 
+@version: 
+@Author: HLLI8
+@Date: 2020-07-22 10:00:48
+@LastEditors: HLLI8
+@LastEditTime: 2020-07-22 10:01:08
+'''
+import sys
+sys.path.append ("D:/ProgramFile/Anaconda/Lib/site-packages") 
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' #只显示warning和Error
+
