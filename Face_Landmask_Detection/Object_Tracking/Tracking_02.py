b50572056234065d6c713a83a8198004bb92ee91
hlli8 <lg95312lg@outlook.com>
2020-07-22 13:49:19 +0800

    物体追踪 封装代码

---
+++ Face_Landmask_Detection/Object_Tracking/Tracking_02.py
@@ -0,0 +1,12 @@
+'''
+@Description: 
+@version: 
+@Author: HLLI8
+@Date: 2020-07-22 13:45:11
+@LastEditors: HLLI8
+@LastEditTime: 2020-07-22 13:48:48
+'''
+import sys
+sys.path.append ("D:/ProgramFile/Anaconda/Lib/site-packages") 
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' #只显示warning和Error
