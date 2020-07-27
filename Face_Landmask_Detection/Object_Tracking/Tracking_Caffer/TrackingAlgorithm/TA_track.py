c2ebf8b93beded56d5af900377addb0c50fbb729
hlli8 <lg95312lg@outlook.com>
2020-07-27 10:21:00 +0800

    BOOSTING MIL KCF TLD MEDIANFLOW GOTURN MOSSE CSRT算法

---
+++ Face_Landmask_Detection/Object_Tracking/Tracking_Caffer/TrackingAlgorithm/TA_track.py
@@ -0,0 +1,14 @@
+'''
+@Description: 
+@version: 
+@Author: HLLI8
+@Date: 2020-07-27 10:19:08
+@LastEditors: HLLI8
+@LastEditTime: 2020-07-27 10:19:23
+'''
+import sys
+sys.path.append ("D:/ProgramFile/Anaconda/Lib/site-packages") 
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' #只显示warning和Error
+
+
