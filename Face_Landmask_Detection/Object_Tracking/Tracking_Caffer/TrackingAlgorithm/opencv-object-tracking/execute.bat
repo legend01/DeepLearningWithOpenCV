4f82bc897dcc13bf5b232b685a5395ba8bb39951
hlli8 <lg95312lg@outlook.com>
2020-07-30 14:10:39 +0800

    cv2.MultiTracker_create() 多目标追踪
    精度有问题

---
+++ Face_Landmask_Detection/Object_Tracking/Tracking_Caffer/TrackingAlgorithm/opencv-object-tracking/execute.bat
@@ -0,0 +1,4 @@
+
+ python .\multi_object_tracking.py --video E:\PythonWorkSpace\DeepLearningWithOpenCV\Face_Landmask_Detection\Object_Tracking\Tracking_Caffer\TrackingAlgorithm\opencv-object-tracking\dashcam_boston.mp4 --tracker kcf
+
+ python .\multi_object_tracking.py --video E:\PythonWorkSpace\DeepLearningWithOpenCV\Face_Landmask_Detection\Object_Tracking\Tracking_Caffer\TrackingAlgorithm\opencv-object-tracking\american_pharoah.mp4 --tracker kcf
--- Face_Landmask_Detection/Object_Tracking/Tracking_Caffer/TrackingAlgorithm/opencv-object-tracking/multi_object_tracking.py
+++ Face_Landmask_Detection/Object_Tracking/Tracking_Caffer/TrackingAlgorithm/opencv-object-tracking/multi_object_tracking.py
@@ -4,7 +4,7 @@
 @Author: HLLI8
 @Date: 2020-07-28 11:26:45
 @LastEditors: HLLI8
-@LastEditTime: 2020-07-28 13:58:38
+@LastEditTime: 2020-07-30 13:57:18
 '''
 import sys
 sys.path.append ("D:/ProgramFile/Anaconda/Lib/site-packages") 
@@ -26,13 +26,13 @@ OPENCV_OBJECT_TRACKERS = {
     "csrt": cv2.TrackerCSRT_create,
     "kcf": cv2.TrackerKCF_create,
     "boosting": cv2.TrackerBoosting_create,
-    "mil": cv2.TrackerMil_create,
+    "mil": cv2.TrackerMIL_create,
     "tld": cv2.TrackerTLD_create,
     "medianflow": cv2.TrackerMedianFlow_create,
     "mosse": cv2.TrackerMOSSE_create
 }
 
-trackers = cv2.MultiTracker_create().
+trackers = cv2.MultiTracker_create()
 
 if not args.get("video", False):
     print("[INFO] starting video stream...")
@@ -40,4 +40,33 @@ if not args.get("video", False):
     time.sleep(1.0)
 else:
     vs = cv2.VideoCapture(args["video"])
+
+while True:
+    frame = vs.read()
+    frame = frame[1] if args.get("video", False) else frame
     
+    if frame is None:
+        break
+    
+    frame = imutils.resize(frame, width=600)
+
+    (success, boxes) = trackers.update(frame)
+    for box in boxes:
+        (x, y, w, h) = [int(v) for v in box]
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0,255,0), 2)
+
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
+
+    if key == ord("s"):
+        box = cv2.selectROI("Frame", frame, fromCenter=False, showCrosshair=True)
+        tracker = OPENCV_OBJECT_TRACKERS[args["tracker"]]()
+        trackers.add(tracker, frame, box)
+    elif key == ord("q"):
+        break
+if not args.get("video", False):
+    vs.stop()
+else:
+    vs.release()
+
+cv2.destroyAllWindows()
